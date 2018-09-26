6a6512cb95ae614bacaf2824952d7d985c86d837
Robson Amorim <robson.amorim@lambda3.com.br>
2018-09-26 11:33:24 -0300

    initial commit

--- DemoCosmos/DemoCosmos/ClubPageViewModel.cs
+++ DemoCosmos/DemoCosmos/ClubPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using MvvmHelpers;
+﻿using Microsoft.Azure.Documents.Client;
+using MvvmHelpers;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -9,6 +13,13 @@ namespace DemoCosmos
 {
     public class ClubPageViewModel : BaseViewModel
     {
+        private readonly string URL = "https://rsamorim.documents.azure.com:443/";
+        private readonly string READ_WRITE_KEY = "gVfE2Gc4AznzfUBUArDdSmSwoevyzemGbIHyhJZe48BQobgagNITDcQ3Te1KycAkk8MC6UR4tAsi9VLKdjcekA==";
+        private readonly string READ_ONLY_KEY = "6EgWliigrdQWj0Fx29z6XEWlbFbHukIcieCYiFYz1ZcfAXWxBC5VetwUP9WSvKARSjKs1jVPfCy3kI4adw9VaA==";
+
+        private readonly Uri DOCUMENT_COLLECTION_URI;
+        private DocumentClient _documentClient;
+
 
         private Club _club;
 
@@ -41,31 +52,53 @@ namespace DemoCosmos
         {
             Club = club;
 
+            _documentClient = new DocumentClient(new Uri(URL), READ_WRITE_KEY);
+            DOCUMENT_COLLECTION_URI = UriFactory.CreateDocumentCollectionUri("SampleCosmos", "Clubs");
 
             IsAdding = string.IsNullOrEmpty(club.id);
             IsUpdating = !string.IsNullOrEmpty(club.id);
 
             SaveCmd = new Command(async () =>
             {
-                if (true)
+                Club.Name = await TransformClubName(Club.Name);
+                var response = await _documentClient.CreateDocumentAsync(DOCUMENT_COLLECTION_URI, Club);
+                if (response.StatusCode == System.Net.HttpStatusCode.Created)
                 {
                     await App.Current.MainPage.Navigation.PushAsync(new MainPage());
-                    await App.Current.MainPage.DisplayAlert("Xamarin Summit 2018", "Save success", "OK");
+                    await App.Current.MainPage.DisplayAlert("SFCI2018", "Save success", "OK");
                 }
                 else
-                    await App.Current.MainPage.DisplayAlert("Xamarin Summit 2018", "Save fail", "OK");
+                    await App.Current.MainPage.DisplayAlert("SFCI2018", "Save fail", "OK");
             });
 
             UpdateCmd = new Command(async () =>
             {
-                if (true)
+                var response = await _documentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri("SampleCosmos", "Clubs", club.id), Club);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
                     await App.Current.MainPage.Navigation.PushAsync(new MainPage());
-                    await App.Current.MainPage.DisplayAlert("Xamarin Summit 2018", "Update success", "OK");
+                    await App.Current.MainPage.DisplayAlert("SFCI2018", "Update success", "OK");
                 }
                 else
-                    await App.Current.MainPage.DisplayAlert("Xamarin Summit 2018", "Update fail", "OK");
+                    await App.Current.MainPage.DisplayAlert("SFCI2018", "Update fail", "OK");
             });
+        }
+
+        public async Task<string> TransformClubName(string clubName)
+        {
+            var url = $"https://scfifunction.azurewebsites.net/" +
+                $"api/SCIFunction?code=FCpza1aE5BalrqnTl7vvXsOM0vEt2AM7rtOZSs8IbrlAbHea3KXVlA==&name={clubName}";
+
+            var client = new HttpClient();
+            try
+            {
+                var result = await client.GetStringAsync(url);
+                return JsonConvert.DeserializeObject<string>(result);
+            }
+            catch (Exception)
+            {
+                return "Error";
+            }
         }
     }
 }
--- DemoCosmos/DemoCosmos/MainPageViewModel.cs
+++ DemoCosmos/DemoCosmos/MainPageViewModel.cs
@@ -1,6 +1,8 @@
-﻿using MvvmHelpers;
+﻿using Microsoft.Azure.Documents.Client;
+using MvvmHelpers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -9,6 +11,12 @@ namespace DemoCosmos
 {
     public class MainPageViewModel : BaseViewModel
     {
+        private readonly string URL = "https://rsamorim.documents.azure.com:443/";
+        private readonly string READ_WRITE_KEY = "gVfE2Gc4AznzfUBUArDdSmSwoevyzemGbIHyhJZe48BQobgagNITDcQ3Te1KycAkk8MC6UR4tAsi9VLKdjcekA==";
+        private readonly string READ_ONLY_KEY = "6EgWliigrdQWj0Fx29z6XEWlbFbHukIcieCYiFYz1ZcfAXWxBC5VetwUP9WSvKARSjKs1jVPfCy3kI4adw9VaA==";
+
+        private readonly Uri DOCUMENT_COLLECTION_URI;
+        private DocumentClient _documentClient;
 
         private ObservableRangeCollection<Club> clubs;
 
@@ -22,7 +30,8 @@ namespace DemoCosmos
 
         public MainPageViewModel()
         {
-
+            _documentClient = new DocumentClient(new Uri(URL), READ_ONLY_KEY);
+            DOCUMENT_COLLECTION_URI = UriFactory.CreateDocumentCollectionUri("SampleCosmos", "Clubs");
 
             AddNewClubCmd = new Command(() =>
             {
@@ -35,7 +44,8 @@ namespace DemoCosmos
         public void GetClubs()
         {
             ClubsList = new ObservableRangeCollection<Club>();
-
+            var clubs = _documentClient.CreateDocumentQuery<Club>(DOCUMENT_COLLECTION_URI).Where(c => c.Name != string.Empty).ToList();
+            ClubsList.AddRange(clubs);
         }
     }
 }
